ec2665047639d8b5e47ada1db583454de52bde7a
leandrorj <leandro__brasil@hotmail.com>
2020-10-06 20:20:34 -0300

    Adicao da tela de resultado

--- mod2-android-nativo-java/calculadora-salario-liquido/app/src/main/java/com/example/calculadorasalarioliquido/MainActivity.java
+++ mod2-android-nativo-java/calculadora-salario-liquido/app/src/main/java/com/example/calculadorasalarioliquido/MainActivity.java
@@ -11,4 +11,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //    public void CliqueBtnCalcular(){
+//
+//        final EditText edtSalarioBrutoMain = (EditText) findViewById(R.id.edtSalarioBrutoMain);
+//        final EditText edtNumDependentesMain = (EditText) findViewById(R.id.edtNumDependentesMain);
+//        final EditText edtOutrosDescontosMain = (EditText) findViewById(R.id.edtOutrosDescontosMain);
+//        Button btnCalcularValoresMain = (Button) findViewById(R.id.btnCalcularValoresMain);
+//
+//        final float floatSalarioBrutoMain = Float.parseFloat(edtSalarioBrutoMain.getText().toString());
+//        final float floatNumDependentesMain = Float.parseFloat(edtNumDependentesMain.getText().toString());
+//        final float floatOutrosDescontosMain = Float.parseFloat(edtOutrosDescontosMain.getText().toString());
+//
+//        btnCalcularValoresMain.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intentMain = new Intent(MainActivity.this, ResultadoFinal.class);
+//                intentMain.putExtra("valorSalarioBrutoMain", floatSalarioBrutoMain);
+//                intentMain.putExtra("valorSalarioBrutoMain", floatNumDependentesMain);
+//                intentMain.putExtra("valorSalarioBrutoMain", floatOutrosDescontosMain);
+//                startActivity(intentMain);
+//            }
+//        });
+//    }
+
 }
+
