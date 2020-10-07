ff59fc0947dbbfe06c81db9facb42ca32e0e7a1d
leandrorj <leandro__brasil@hotmail.com>
2020-10-06 21:28:35 -0300

    Foi adicionado o codigo para retornar ao menu principal e foi realizado a captura dos dados na tela de resultado

--- mod2-android-nativo-java/calculadora-salario-liquido/app/src/main/java/com/example/calculadorasalarioliquido/MainActivity.java
+++ mod2-android-nativo-java/calculadora-salario-liquido/app/src/main/java/com/example/calculadorasalarioliquido/MainActivity.java
@@ -2,38 +2,56 @@ package com.example.calculadorasalarioliquido;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String VALOR_SALARIO_BRUTO = "valorSalarioBrutoMain";
+    public static final String VALOR_NUMERO_DEPENDENTES = "valorNumDependentesMain";
+    public static final String VALOR_OUTROS_DESCONTOS = "valorOutrosDescontosMain";
+
+    EditText edtSalarioBrutoMain;
+    EditText edtNumDependentesMain;
+    EditText edtOutrosDescontosMain;
+    Button btnCalcularValoresMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CliqueBtnCalcular();
     }
 
-    //    public void CliqueBtnCalcular(){
-//
-//        final EditText edtSalarioBrutoMain = (EditText) findViewById(R.id.edtSalarioBrutoMain);
-//        final EditText edtNumDependentesMain = (EditText) findViewById(R.id.edtNumDependentesMain);
-//        final EditText edtOutrosDescontosMain = (EditText) findViewById(R.id.edtOutrosDescontosMain);
-//        Button btnCalcularValoresMain = (Button) findViewById(R.id.btnCalcularValoresMain);
-//
-//        final float floatSalarioBrutoMain = Float.parseFloat(edtSalarioBrutoMain.getText().toString());
-//        final float floatNumDependentesMain = Float.parseFloat(edtNumDependentesMain.getText().toString());
-//        final float floatOutrosDescontosMain = Float.parseFloat(edtOutrosDescontosMain.getText().toString());
-//
-//        btnCalcularValoresMain.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intentMain = new Intent(MainActivity.this, ResultadoFinal.class);
-//                intentMain.putExtra("valorSalarioBrutoMain", floatSalarioBrutoMain);
-//                intentMain.putExtra("valorSalarioBrutoMain", floatNumDependentesMain);
-//                intentMain.putExtra("valorSalarioBrutoMain", floatOutrosDescontosMain);
-//                startActivity(intentMain);
-//            }
-//        });
-//    }
+    private void CliqueBtnCalcular(){
 
+        edtSalarioBrutoMain = findViewById(R.id.edt_salario_bruto_main);
+        edtNumDependentesMain = findViewById(R.id.edt_numero_dependente_main);
+        edtOutrosDescontosMain = findViewById(R.id.edt_outros_dependentes_main);
+        btnCalcularValoresMain = findViewById(R.id.btn_calcular_tela_main);
+
+        btnCalcularValoresMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            if (edtSalarioBrutoMain.getText().length() > 0) {
+                Intent intentMain = new Intent(MainActivity.this, ResultadoSalarioFinal.class);
+                intentMain.putExtra(VALOR_SALARIO_BRUTO, edtSalarioBrutoMain.getText().toString());
+                intentMain.putExtra(VALOR_NUMERO_DEPENDENTES, edtNumDependentesMain.getText().toString().equals("") ? "0" : edtNumDependentesMain.getText().toString());
+                intentMain.putExtra(VALOR_OUTROS_DESCONTOS, edtOutrosDescontosMain.getText().toString().equals("") ? "0" : edtOutrosDescontosMain.getText().toString());
+                startActivity(intentMain);
+
+            } else {
+                new AlertDialog.Builder(MainActivity.this).setTitle("Erro")
+                        .setMessage("Por favor, preencha ao menos o campo de 'Salário Bruto'.")
+                        .setNeutralButton("OK", null).show();
+            }
+            }
+        });
+    }
 }
 
--- mod2-android-nativo-java/calculadora-salario-liquido/app/src/main/java/com/example/calculadorasalarioliquido/ResultadoSalarioFinal.java
+++ mod2-android-nativo-java/calculadora-salario-liquido/app/src/main/java/com/example/calculadorasalarioliquido/ResultadoSalarioFinal.java
@@ -2,13 +2,53 @@ package com.example.calculadorasalarioliquido;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Objects;
 
 public class ResultadoSalarioFinal extends AppCompatActivity {
+    double salarioBruto = 0;
+    int numeroDependentes = 0;
+    double outrosDescontos = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado_salario_final);
+
+        ReceberDadosMain();
+
+
+
+        RetornarTelaPrincipal();
     }
+
+
+    private void ReceberDadosMain() {
+        Intent intent = getIntent();
+        if (intent != null) {
+            salarioBruto = Double.parseDouble(Objects.requireNonNull(intent.getStringExtra(MainActivity.VALOR_SALARIO_BRUTO)));
+            numeroDependentes = Integer.parseInt(Objects.requireNonNull(intent.getStringExtra(MainActivity.VALOR_NUMERO_DEPENDENTES)));
+            outrosDescontos = Double.parseDouble(Objects.requireNonNull(intent.getStringExtra(MainActivity.VALOR_OUTROS_DESCONTOS)));
+        }
+    }
+
+
+
+    private void RetornarTelaPrincipal(){
+        Button btnVoltarParaTelaPrincipal;
+        btnVoltarParaTelaPrincipal = findViewById(R.id.btn_voltar_tela_resultado);
+
+        btnVoltarParaTelaPrincipal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent returnMain = new Intent(ResultadoSalarioFinal.this, MainActivity.class);
+                startActivity(returnMain);
+            }
+        });
+    }
+
 }
