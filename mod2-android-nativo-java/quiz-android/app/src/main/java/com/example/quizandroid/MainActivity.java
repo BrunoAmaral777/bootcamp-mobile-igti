596096543a0f659fa6e4c4c7db21b0637debc925
leandrorj <leandro__brasil@hotmail.com>
2020-10-07 17:52:41 -0300

    Foi adicionado a sub HabilitarFullScreen e retirado os arquivos nao utilizados

--- mod2-android-nativo-java/quiz-android/app/src/main/java/com/example/quizandroid/MainActivity.java
+++ mod2-android-nativo-java/quiz-android/app/src/main/java/com/example/quizandroid/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.quizandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        HabilitarFullScreen();
+
         setContentView(R.layout.activity_main);
+    }
+
+    private void HabilitarFullScreen(){
+        // remove title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Remove a ActionBar da Activity
+        getSupportActionBar().hide();
     }
 }
