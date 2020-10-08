7b04e153b60b4711a9acd5bc066260b8996e62c7
leandrorj <leandro__brasil@hotmail.com>
2020-10-08 10:48:45 -0300

    adicao das questoes e criacao da classe question

---
+++ mod2-android-nativo-java/quiz-android/app/src/main/java/com/example/quizandroid/Questions.java
@@ -0,0 +1,7 @@
+package com.example.quizandroid;
+
+public class Questions {
+
+
+
+}
--- mod2-android-nativo-java/quiz-android/app/src/main/java/com/example/quizandroid/QuizActivity.java
+++ mod2-android-nativo-java/quiz-android/app/src/main/java/com/example/quizandroid/QuizActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -16,10 +17,36 @@ public class QuizActivity extends AppCompatActivity {
         getSupportActionBar().hide(); // Remove a ActionBar da Activity
         setContentView(R.layout.activity_quiz);
 
+        ClickBtnVerdadeiro();
+        ClickBtnFalso();
         MostrarResultsActivity();
     }
 
 
+    private void ClickBtnVerdadeiro(){
+        Button btnVerdadeiro;
+        btnVerdadeiro = findViewById(R.id.btnVerdadeiro);
+
+        btnVerdadeiro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(QuizActivity.this,R.string.correct_toast,Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    private void ClickBtnFalso(){
+        Button btnFalso;
+        btnFalso = findViewById(R.id.btnFalso);
+
+        btnFalso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(QuizActivity.this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
 
     private void MostrarResultsActivity() {
